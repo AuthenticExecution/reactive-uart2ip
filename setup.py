bd3c09c7b5b79647f6245dd21e2a946654d8f2a2
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-01-08 11:00:46 +0100

    fix versions of deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='reactive-uart2ip',
     version='0.1',
     packages=find_packages(),
-    install_requires=['colorlog', 'pyserial-asyncio'],
+    install_requires=['colorlog==4.6.2', 'pyserial-asyncio==0.5'],
     entry_points={
         'console_scripts': ['reactive-uart2ip = uart2ip.main:main']
     },
