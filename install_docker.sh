df514772481def0b87fdcf2fe4698c62734c14fa
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-05-03 10:54:26 +0200

    fix branch of reactive-net dep

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y git
-git clone https://github.com/gianlu33/reactive-net.git
+git clone --branch fosdem-21 https://github.com/gianlu33/reactive-net.git
 
 pip install reactive-net/
 pip install reactive-uart2ip/
